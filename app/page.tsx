30a8888161f235ae6fe8fb17a597b746aac744a7
Muthu Raja <contact@bytecraftstudio.com>
2026-06-13 09:50:19 +0530

    Updated submit button

--- app/page.tsx
+++ app/page.tsx
@@ -391,8 +391,7 @@ const [message, setMessage] = useState("");
             </p>
           </div>
 
-      <div className="bg-white/5 border border-white/10 rounded-[40px] p-10 backdrop-blur-xl flex items-center justify-center min-h-[350px]">
-
+      <div className="flex items-center justify-center">
   <a
     href="https://docs.google.com/forms/d/e/1FAIpQLSeD-9Tuh-nEln__MdQWZqqC84k7yC-f_YVzXXir0V3cU2djrg/viewform?usp=header"
     target="_blank"
